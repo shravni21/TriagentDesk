89293b95ab4c766b609d4686f38611ce28241972
shravni21 <wakdeshravni1@gmail.com>
2025-12-06 19:26:36 +0530

    Let's try in MERN

--- TriagentDesk_backend/src/main/java/com/shravniWakde/TriagentDesk/service/ProfileService.java
+++
@@ -1,9 +0,0 @@
-package com.shravniWakde.TriagentDesk.service;
-
-import com.shravniWakde.TriagentDesk.io.ProfileRequest;
-import com.shravniWakde.TriagentDesk.io.ProfileResponse;
-
-public interface ProfileService {
-     ProfileResponse createProfile(ProfileRequest profileRequest);
-
-}
